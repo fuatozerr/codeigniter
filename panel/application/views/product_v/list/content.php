8d02412c592f2ce10386b08cd64052a1868a9b28
fuatozerr <fuatozerr23@gmail.com>
2018-06-22 15:09:22 +0300

    Ürünler modülünün listesi için html sablon olusturuldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,35 @@
-ürün listesi ...
+<div class="row">
+
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <h4 class="m-b-lg">Ürünleri Listele</h4>
+
+
+            <table class="table table-hover ">
+                <thead>
+                <th>#id</th>
+                <th>Url</th>
+                <th>Başlık</th>
+
+                <th>Açıklama</th>
+
+                <th>Durum</th>
+                <th>İşlem</th>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <td>#1</td>
+                    <td>araba</td>
+                    <td>porche</td>
+                    <td>kaçırma bedava</td>
+                    <td>aktif</td>
+                    <td><a href="#" class="btn btn-sm btn-outline btn-danger"><i class="fa fa-warning"></i>Sil</a>
+                        <a href="#" class="btn btn-sm btn-outline btn-primary"><i class="fa fa-pencil-square-o"></i>Güncelle</a></td>
+                </tr>
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+
+</div>
