c74317136dcd544b5b5491215c7074a20586fce9
fuatozerr <fuatozerr23@gmail.com>
2018-06-22 15:28:20 +0300

    Toogle butonu eklendi. Html tamamen bitti

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -2,9 +2,12 @@
 
     <div class="col-md-12">
         <div class="widget p-lg">
-            <h4 class="m-b-lg">Ürünleri Listele</h4>
-
-
+            <h4 class="m-b-lg">Ürünleri Listele
+                <a href="#" class="btn btn-outline btn-primary btn-sm pull-right"><i class="fa fa-plus"></i> Yeni Ekle</a></h4>
+    
+            <div class="alert alert-info text-center">
+                <p>Kayıt bulunamadı. Veritabanından çekince burası otomatik silinecek.</p>
+            </div>
             <table class="table table-hover ">
                 <thead>
                 <th>#id</th>
@@ -23,7 +26,7 @@
                     <td>araba</td>
                     <td>porche</td>
                     <td>kaçırma bedava</td>
-                    <td>aktif</td>
+                    <td><input id="switch-2-2" type="checkbox" data-switchery data-color="#01c469" checked /></td>
                     <td><a href="#" class="btn btn-sm btn-outline btn-danger"><i class="fa fa-warning"></i>Sil</a>
                         <a href="#" class="btn btn-sm btn-outline btn-primary"><i class="fa fa-pencil-square-o"></i>Güncelle</a></td>
                 </tr>
